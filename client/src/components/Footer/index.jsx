369826c5329b6015db782c07ad26dd672e2dea57
Jeorge Donato <jeorgekhenr@gmail.com>
2021-03-31 21:09:01 -0700

    Added footer

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.scss'
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='container-fluid'>
+          <span>Copyright 2021</span>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import './style.scss'
 
 const Navbar = () => {
   return (
-    <nav className='navbar navbar-light bg-light'>
+    <nav className='navbar navbar-expand-lg navbar-light'>
       <div className='container-fluid'>
-        <a className='navbar-brand' href='#'>
-          <img src='/docs/5.0/assets/brand/bootstrap-logo.svg' alt='' width='30' height='24' className='d-inline-block align-top' />
-          Shopazon
-        </a>
+        <a className='navbar-brand text-light' href='#'>Shopazon</a>
+        <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNavAltMarkup' aria-controls='navbarNavAltMarkup' aria-expanded='false' aria-label='Toggle navigation'>
+          <span className='navbar-toggler-icon' />
+        </button>
+        <div className='collapse navbar-collapse ' id='navbarNavAltMarkup'>
+          <div className='navbar-nav'>
+            <a className='nav-link active' aria-current='page' href='#'>Home</a>
+            <a className='nav-link' href='#'>Features</a>
+            <a className='nav-link' href='#'>Pricing</a>
+            <a className='nav-link disabled' href='#' tabindex='-1' aria-disabled='true'>Disabled</a>
+          </div>
+        </div>
       </div>
     </nav>
   )
